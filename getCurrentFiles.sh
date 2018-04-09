51c5fb39b5ce6c639c3a6058e0524f4c2f7d295a
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2018-04-09 14:46:54 +0200

    flake8

--- getCurrentFiles.sh
+++ getCurrentFiles.sh
@@ -26,6 +26,8 @@ then
 	git diff ./ipython_config_$HOST.py ../.ipython/profile_default/ipython_config.py
 	echo git diff ./_gitconfig ../.gitconfig
 	git diff ./_gitconfig ../.gitconfig
+	echo git diff ./flake8 ../.config/flake8
+	git diff ./flake8 ../.config/flake8
 	echo diff -r ./_vim/after ../.vim/after
 	diff -r ./_vim/after ../.vim/after
 	read -p "##### Are you sure you want to get the config files? (y/Y sets config, all else stops): " choice
@@ -35,13 +37,14 @@ then
 		  ;;
 	  * ) exit;;
 	esac
-	echo _GETTING_ .zshrc, .vimrc, .gitconfig, .tmux, .ipython config and the folder .vim/after from home directory
+	echo _GETTING_ .zshrc, .vimrc, .gitconfig, config/flake8, .tmux, .ipython config and the folder .vim/after from home directory
 	cp ../.zshrc ./_zshrc_$HOST
 	cp ../.vimrc ./_vimrc_$HOST
 	cp ../.tmux.conf ./_tmux.conf_$HOST
 	cp ../.ssh/config ./_ssh_config_$HOST
 	cp ../.ipython/profile_default/ipython_config.py ./ipython_config_$HOST.py
 	cp ../.gitconfig ./_gitconfig
+	cp ../.config/flake8 ./flake8
 	mkdir -p ./_vim/after
 	cp -r ../.vim/after/ ./_vim/
 elif [ "$1" = "set" ]
@@ -58,6 +61,8 @@ then
 	git diff ../.ipython/profile_default/ipython_config.py ./ipython_config_$HOST.py
 	echo git diff ../.gitconfig ./_gitconfig
 	git diff ../.gitconfig ./_gitconfig
+	echo git diff ../.config/flake8 ./flake8
+	git diff ../.config/flake8 ./flake8
 	echo diff -r ../.vim/after ./_vim/after
 	diff -r ../.vim/after ./_vim/after
 	read -p "##### Are you sure you want to set the config files? (y/Y sets config, all else stops): " choice
@@ -67,13 +72,14 @@ then
 		  ;;
 	  * ) exit;;
 	esac
-	echo _SETTING_ .zshrc, .vimrc, .gitconfig, .tmux, ipython config and the folder .vim/after from home directory
+	echo _SETTING_ .zshrc, .vimrc, .gitconfig, .config/flake8, .tmux, ipython config and the folder .vim/after from home directory
 	cp ./_zshrc_$HOST ../.zshrc
 	cp ./_vimrc_$HOST ../.vimrc
 	cp ./_tmux.conf_$HOST ../.tmux.conf
 	cp ./_ssh_config_$HOST ../.ssh/config
 	cp ./ipython_config_$HOST.py ../.ipython/profile_default/ipython_config.py
 	cp ./_gitconfig ../.gitconfig
+	cp ./flake8 ../.config/flake8
 	mkdir -p ../.vim/after
 	cp -r ./_vim/after ../.vim/
 elif [ "$1" = "comp" ]
