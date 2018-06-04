29b298a4ee88f18e825c9f4fc329fc4e59a60cd1
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2018-06-04 13:00:40 +0200

    multiple scripts per job with repeatsbatch

--- toolbox/repeatSbatchXTimes.sh
+++ toolbox/repeatSbatchXTimes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-functionCall='sbatch -p '
+functionCall='sbatch -p ' # --begin=now+6hours  # if one wants the jobs to start later
 # if third argument 
 if [ $# -gt 3 ]; then
 	functionCall=$functionCall'experiment --wmod '$3' --hicann '$4' '
@@ -9,18 +9,26 @@ elif [ $# -gt 2 ]; then
 else
 	functionCall=$functionCall'simulation '
 fi
-functionCall=$functionCall'<(echo -n "#"!; echo /bin/sh; echo '
-functionCall=$functionCall$1
-functionCall=$functionCall');'
+functionCall=$functionCall'<(echo -n "#"!; echo /bin/sh; '
 # functionCall="echo Hello, World!; cat <(echo sadasdsadasas)"
-echo "are you sure you want to submit '"$functionCall"' "$2" times?"
-read -p "##### do it (y/Y): " choice
-case "$choice" in 
-  y|Y ) 
-	  echo "   Continuing..."
-	  ;;
-  * ) exit;;
+echo "are you sure you want to submit '"$functionCall"echo "$1"[*repetitions]);' "$2" times?"
+read -p "what should be the repetition per job (everything but a number will quit): " numOfFilesPerJob
+case $numOfFilesPerJob in
+	''|*[!0-9]*) exit ;;
+	*) echo continue... ;;
 esac
+while (( $numOfFilesPerJob > 0 )); do
+	functionCall=$functionCall"echo "$1";"
+	numOfFilesPerJob=$(($numOfFilesPerJob-1))
+done
+functionCall=$functionCall');'
+#read -p "##### do it (y/Y): " choice
+#case "$choice" in 
+#  y|Y ) 
+#	  echo "   Continuing..."
+#	  ;;
+#  * ) exit;;
+#esac
 step=$2
 while [ $step -gt 0 ]; do
 	eval "${functionCall}"
