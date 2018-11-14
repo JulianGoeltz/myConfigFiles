c0e5de62bbb9746088a9a1d1c186dac35a82972a
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2018-11-14 14:53:44 +0100

    small stuff

--- toolbox/lock.sh
+++ toolbox/lock.sh
@@ -5,7 +5,8 @@
 pkill -u $USER -USR1 dunst
 # previously: 
 # i3lock -tbefi /home/julgoe/Pictures/actualBackgrounds/lock_1820.png -c 000000
-i3lock -nte -i /home/julgoe/Pictures/actualBackgrounds/lock_1820.png \
+# -i /home/julgoe/Pictures/actualBackgrounds/lock_1820.png \
+i3lock -nte -B4 \
     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
     --insidevercolor=000000ff --insidewrongcolor=d23c3dff \
--- toolbox/screenSet.sh
+++ toolbox/screenSet.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+if [ $# -gt 0 ]; then
+	# set up three monitor setup
+	notify-send -t 3000 "setting xrandr in office special"
+	xbacklight -set 100
+	xrandr --output DP2-3 --mode 1920x1080 --primary
+	xrandr --output eDP1 --off
+	xrandr --output eDP1 --mode 1920x1080 --left-of DP2-3
+	xrandr --output DP2-2 --mode 1920x1080 --right-of DP2-3
+	exit
+fi
+
 if xrandr | grep -q 'DP2-2 connected' &&
 	[ "$(xrandr | grep --after-context=1 'DP2-2' | grep -c DP2)" -eq 1 ]; then
 	# second condition because it takes time to adapt to dockingstation
 	# Connected to Docking Station
-	notify-send "setting xrandr for dockingstation in office"
+	notify-send -t 3000 "setting xrandr for dockingstation in office"
 	# check if already set
 	if xrandr | grep "eDP1" | grep -q "+0+" && 
 		xrandr | grep "DP2-2" | grep -q "+1920+" &&
@@ -12,6 +23,7 @@ if xrandr | grep -q 'DP2-2 connected' &&
 		notify-send "xrandr already set, exiting."
 		exit
 	fi
+	xbacklight -set 10
 	xrandr --output VIRTUAL1 --off 
 	xrandr --output DP2-2 --off
 	xrandr --output DP2-3 --off
@@ -19,17 +31,16 @@ if xrandr | grep -q 'DP2-2 connected' &&
 	xrandr --output DP2-3 --mode 1920x1080 --same-as eDP1
 	# maybe turn DP2-3 off and on again (in xrandr)
 	# turn down screen brightness on edp1
-	xbacklight -set 10
 else
 	# assume no other display connected, use only laptop
-	notify-send "setting xrandr for mobile use"
+	xbacklight -set 100
+	notify-send -t 3000 "setting xrandr for mobile use"
 	xrandr --output eDP1 --auto --primary
 	xrandr --output DP2-2 --off
 	xrandr --output DP2-3 --off
 	#xrandr --output DP2-2 --same-as eDP1 || xrandr --output DP2-2 --off
 	#xrandr --output DP2-3 --same-as eDP1 || xrandr --output DP2-3 --off
 	# turn up screen brightness again
-	xbacklight -set 100
 fi
 
 #cat /etc/systemd/logind.conf
