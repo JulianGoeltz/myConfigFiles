614debd4414d28fc3fde77a7f995430e74015cb0
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2019-08-28 15:35:36 +0200

    sweepWafer with adapted licenses, also longexp queue

--- toolbox/sweepWafer.sh
+++ toolbox/sweepWafer.sh
@@ -3,10 +3,14 @@
 set -euo pipefail
 
 if [ $# -eq 0 ]; then
-	echo "first argument is string of wafers, separated by whitespaces"
-	echo "if second argument is not an existing file it is taken as the rep number"
-	echo "all other arguments are the files where the following call is executed"
-	echo '    sbatch -p experiment --time 1-0:0:0 --wmod $wafer --wrap "singularity exec --app visionary-wafer /containers/stable/latest ./train.py train_yccp $file"'
+	cat <<-EOF
+		first argument is string of wafers, separated by whitespaces
+		if second argument is not an existing file it is taken as the rep number
+		all other arguments are the files where the following call is executed
+		    sbatch -p experiment --time 12:0:0 --wmod \$wafer --wrap "run_nmpm_software ./train.py train_yccp \$file"
+
+		    if $partition is in environment, it is used instead of `experiment`
+		EOF
 	exit
 fi
 
@@ -17,8 +21,9 @@ fi
 
 # define licences wafer-specific
 hicann_wafer=(
-	[33]="271,239,272,240,299,322,323,301"
-	[37]="271,239,299,322,323,301"
+	[30]="269,271,239,299,322,323,301"
+	[33]="266,267,235,299,322,323,301"
+	[37]="271,239,203,299,322,323,301"
 	)
 
 waferString=$1
@@ -41,8 +46,8 @@ for wafer in $(echo "$waferString" | grep -oP "\S*"); do
 			# containerPath=/containers/stable/latest
 			# containerPath=/containers/stable/2019-01-11_1.img
 			# exclude nodes with `-x HBPHost9 \`
-			output=$(OMP_NUM_THREADS=48 sbatch -p experiment \
-					--time 1-0:0:0 \
+			output=$(OMP_NUM_THREADS=48 sbatch -p "${partition:-experiment}" \
+					--time 12:0:0 \
 					--wmod "$wafer" \
 					-c8 \
 					--mem 10g \
