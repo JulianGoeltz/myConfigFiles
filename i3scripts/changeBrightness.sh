931bea5cbfde472b7e419b2704556bb56a48a6ce
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2019-10-09 01:34:23 +0200

    brightness notifs and updates

--- i3scripts/changeBrightness.sh
+++ i3scripts/changeBrightness.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
-# set -euo pipefail
-
-# in case one is in tty without x:
-# $ su
-# $ cd /sys/class/backlight/intel-backlight
-# $ echo # > brightness
-# # the number # can be 0-100
 case $1 in
 	'up') 
 		xbacklight +20
@@ -22,22 +16,23 @@ case $1 in
 		;;
 esac
 
-
-round() {
-	echo $((($1 + $2/2)/$2))
-}
 # xbacklight takes a bit to reflect change (default time 200ms)
 # sleep 0.4
 
-# if larger than 5 round to nearest 10:
+blockLeft=▕
+blockRight=▏
+blockBlank=⠀
+blockFull=█
+
+brightness=🔅
+
 num=$(xbacklight | xargs printf '%.*f' 0)
 if [ "$num" -gt 0 -a "$num" -lt 5 ]; then
 	num=1
 else
-	#num=$(($(round $num 10)*10))
 	num=$((num / 5))
 fi
 
-bar=$(seq -s "─" 0 $num | sed 's/[0-9]//g')
+bar="$brightness$blockLeft$( echo "$(seq -s "$blockFull" 0 $num)$(seq -s "$blockBlank" $num 20 )" | tr -d 0,1,2,3,4,5,6,7,8,9)$blockRight"
 
 dunstify -i display-brightness-symbolic -r 3333 "$bar"
