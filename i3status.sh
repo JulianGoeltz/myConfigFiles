9e848125f66d77465ad6b57415d96ec71108d856
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2020-01-27 19:33:44 +0100

    ice information in status line

--- i3status.sh
+++ i3status.sh
@@ -69,6 +69,25 @@ Wifi() {
 		signalstr=$((signalstr < 0 ? 0 : signalstr))
 		printf " at%4u%%" $signalstr
 	fi
+	# in ICEs show some fun information
+	if [ "$(echo "$string" | grep -oP "SSID: \K.*")" = "WIFIonICE" ]; then
+		tmp=$(curl -s 'https://portal.imice.de/api1/rs/status')
+		if [ "$?" -eq 0 ]; then
+			json_result=$(echo "$tmp" | 
+				python -c 'import json, sys; data = json.load(sys.stdin); print(" [{}km/h, {} class]".format(data["speed"], "2." if data["wagonClass"] == "SECOND" else "1."))')
+			if [ "$?" -eq 0 ]; then
+				# perhaps do it in full python with
+				# import requests
+				# r = requests.get('https://portal.imice.de/api1/rs/status')
+				# # potentially r.json()
+				echo "$json_result"
+			else
+				echo "[connected to ICE: error with json]"
+			fi
+		else
+			echo "[connected to ICE: error connecting to portal]"
+		fi
+	fi
 }
 # vpn
 Vpn() {
