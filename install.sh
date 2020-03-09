6c5a9f628c348d877e2cd8c876bfafbd856654fc
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2020-03-09 14:30:53 +0100

    restruct install.sh

--- install.sh
+++ install.sh
@@ -3,26 +3,35 @@
 # set -euo pipefail
 LocOfScript=$(dirname "$(readlink -f "$0")")
 
-echo "First, all submodules are initted/updated"
-tmpDir=$(pwd)
-cd "$LocOfScript"
-git submodule update --init
-cd "$tmpDir"
-echo -e "\n\n\n\n\n"
-
-echo "This script will try to link the files, but not replace existing files (no -f force). If there are problems, remove the existing files by hand. This way it is safer."
+# check debug options
+[ "$1" == "--debug" ] && debug=true || debug=false
 
 # define colours for better output
-RED='\033[0;31m'
-# existing and correct, yellow
-ExCorr='\033[0;33m'
-# action correct, green
-ActCorr='\033[0;32m'
-NC='\033[0m'
+COLOUR_NON='\033[0m'
+COLOUR_RED='\033[0;31m'
+COLOUR_ORA='\033[0;33m'
+COLOUR_GRE='\033[0;32m'
+echo_red (){ echo -en "${COLOUR_RED}"; echo $1; echo -en "${COLOUR_NON}";}
+echo_gre (){ echo -en "${COLOUR_GRE}"; echo $1; echo -en "${COLOUR_NON}";}
+echo_ora (){ $debug || return; echo -en "${COLOUR_ORA}"; echo $1; echo -en "${COLOUR_NON}";}
 
 # if something goes wrong, change this variable
 allWentThrough=true
 
+# evalling a command
+evalAndCheckReturn (){
+	[ $# -gt 1 ] && expl="${2} (${1})" || expl=$1
+	retStr=$(eval "$1" 2>&1)
+	retVal=$?
+	if [ "$retVal" -ne 0 ]; then
+		echo_red "${expl}: ${retVal} (${retStr})"
+		allWentThrough=false
+	else
+		echo_gre "${expl}: Ok"
+	fi
+}
+
+# linking files
 checkSimilaritiesAndLink (){
 	orig=$LocOfScript/$1
 	dest=$2
@@ -33,66 +42,81 @@ checkSimilaritiesAndLink (){
 	# check if given dest is a link
 	if [ -L "$dest" ]; then
 		if [[ "$(readlink -f "$dest")" != "$orig" ]]; then
-			echo -e "${RED}File $dest is a link to $(readlink -f "$dest") and not $orig. Delete manually."
+			echo_red "File $dest is a link to $(readlink -f "$dest") and not $orig. Delete manually."
 			allWentThrough=false
 		else
-			echo -e "${ExCorr}File $dest is already linked correctly to $orig."
+			echo_ora "File $dest is already linked correctly to $orig."
 		fi
 	# check if given dest is a file
 	elif [ -f "$dest" ]; then
 		if [ "$4" = "nolink"  ]; then
 			if diff "$orig" "$dest" >/dev/null; then
-				echo -e "${ExCorr}File contents of $orig and $dest are the same."
+				echo_ora "File contents of $orig and $dest are the same."
 			else
 				tmpStr=$(cp "$orig" "$dest" 2>&1)
 				if [ "$?" -ne "0" ]; then
-					echo -e "${RED}$tmpStr"
+					echo_red "$tmpStr"
 					if [ "$3" = "sudo"  ]; then
-						echo -e "${RED} Tried and failed to replace $dest with copy of $orig, try again with sudo BUT CHECK DIFFS BEFORE!!"
+						echo_red "Tried and failed to replace $dest with copy of $orig, try again with sudo BUT CHECK DIFFS BEFORE!!"
 					fi
 					allWentThrough=false
 				else
-					echo -e "${ActCorr} Replaced $dest with $orig."
+					echo_gre "Replaced $dest with $orig."
 				fi
 			fi
 		else
-			echo -e "${RED}File $dest is a regular file, delete manually."
+			echo_red "File $dest is a regular file, delete manually."
 			allWentThrough=false
 		fi
 	# do the linking
 	else
 		if [ -e "$dest" ]; then
-			echo -e "${RED}File $dest is neither link nor regular file, but still exists. Check manually."
+			echo_red "File $dest is neither link nor regular file, but still exists. Check manually."
 			allWentThrough=false
 		else
 			if [ "$4" = "nolink"  ]; then
 				tmpStr=$(cp "$orig" "$dest" 2>&1)
 				if [ "$?" -ne "0" ]; then
-					echo -e "${RED}$tmpStr"
+					echo_red "$tmpStr"
 					if [ "$3" = "sudo"  ]; then
-						echo -e "${RED} Tried and failed to make a copy of $orig at $dest, try again with sudo."
+						echo_red "Tried and failed to make a copy of $orig at $dest, try again with sudo."
 					fi
 					allWentThrough=false
 				else
-					echo -e "${ActCorr}Made a copy of $orig at $dest."
+					echo_gre "Made a copy of $orig at $dest."
 				fi
 			else
 				tmpStr=$(ln -sv "$orig" "$dest" 2>&1)
 				if [ "$?" -ne "0" ]; then
-					echo -e "${RED}$tmpStr"
+					echo_red "Str"
 					if [ "$3" = "sudo"  ]; then
-						echo -e "${RED} Tried and failed to replace $dest with link to $orig, try again with sudo."
+						echo_red "Tried and failed to replace $dest with link to $orig, try again with sudo."
 					fi
 					allWentThrough=false
 				else
-					echo -e "${ActCorr}$tmpStr"
+					echo_gre "$tmpStr"
 				fi
 			fi
 		fi
 	fi
-	echo -en "$NC"
+	# echo -en "$NC"
 
 }
+
+
+echo "This script will setup the config. For that, it will try to link the files, but not replace existing files (no -f force). If there are problems, remove the existing files by hand. This way it is safer."
+echo -e "\n\n"
+
+
+
+tmpDir=$(pwd)
+cd "$LocOfScript"
+evalAndCheckReturn "git submodule update --init" "Initting/updating submodules"
+evalAndCheckReturn "git config core.hooksPath .githooks" "setting hooksPath"
+cd "$tmpDir"
+echo -e "\n\n"
+
+
 
 echo "--ZSH"
 checkSimilaritiesAndLink zshrc "$HOME/.zshrc"
@@ -108,7 +132,7 @@ if [[ "$(hostname)" == "T2" ]]; then
 	[ -h "$HOME/.ssh/config_prox" ] && rm "$HOME/.ssh/config_prox"
 	cat "$HOME/.ssh/config" > "$HOME/.ssh/config_prox"
        	cat "$LocOfScript/ssh_config_proxiedgithub" >> "$HOME/.ssh/config_prox"
-	echo -e "${ActCorr}Combined ~/.ssh/config with local config_prox${NC}"
+	echo_gre "Combined ~/.ssh/config with local config_prox"
 elif [[ "$(hostname)" == "helvetica" ]]; then
 	checkSimilaritiesAndLink ssh_config_proxiedgithub "$HOME/.ssh/config"
 	checkSimilaritiesAndLink ssh_rc "$HOME/.ssh/rc"
@@ -171,8 +195,6 @@ if [[ "$(hostname)" == "T2" ]]; then
 	checkSimilaritiesAndLink pscircle.service "$HOME/.config/systemd/user/pscircle.service"
 
 	checkSimilaritiesAndLink zprofile "$HOME/.zprofile"
-
-	checkSimilaritiesAndLink ensureUpdates_pre-commit "$LocOfScript/.git/hooks/pre-commit"
 
 	echo "----sudos"
 
