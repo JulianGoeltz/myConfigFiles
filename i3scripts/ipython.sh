02b1143b1ef372b118f2730ea0956691362063c4
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2020-05-27 17:21:07 +0200

    i3 changes (ipython start, qc35 profile)

---
+++ i3scripts/ipython.sh
@@ -0,0 +1,4 @@
+#!zsh
+
+source /home/julgoe/venvs/pytorch/bin/activate
+ipython -c "import numpy as np, matplotlib.pyplot as plt, torch" -i
---
+++ i3scripts/qc35_profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+declare -a  mac_array=("2C_41_A1_07_D2_28" "04_52_C7_34_22_DB" )
+
+# check if bluez connected
+status="$(pactl list sinks short | grep bluez)"
+if [ -z "$status" ]; then
+	notify-send "no bluez connected"
+	return
+fi
+
+# notify-send "$status"
+
+# loop through mac addresses
+for mac in "${mac_array[@]}"; do
+	# determine if given
+	# substatus="$(echo status | grep $mac)"
+	substatus="$(pactl list sinks short | grep $mac)"
+	if [ -z "$substatus" ]; then
+		# notify-send "mac '${mac}' not in status"
+		# notify-send "substatus: $substatus"
+		continue
+	fi
+	# determine what current status is
+	if echo "$substatus" | grep headset_head_unit; then
+		pactl set-card-profile bluez_card.$mac a2dp_sink
+	else
+		pactl set-card-profile bluez_card.$mac headset_head_unit
+	fi
+	return
+done
