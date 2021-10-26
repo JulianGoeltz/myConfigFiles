f96a65a02ca504870d9d3c592aa66a4f39c50ceb
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2021-10-26 13:36:30 +0200

    first version for db wifi notifications

--- i3scripts/db_wifi.py
+++ i3scripts/db_wifi.py
@@ -2,8 +2,16 @@
 import datetime
 import json
 import requests
+import subprocess
 import sys
+import time
 
+
+# give notification if arrival within next x minutes for the following stops:
+notification_minutes = 5
+notification_stops = [
+    "Mannheim Hbf",
+]
 
 if len(sys.argv) == 1 or sys.argv[1] == 'ice':
     base = 'https://portal.imice.de/api1/rs/'
@@ -22,6 +30,25 @@ if len(sys.argv) == 1 or sys.argv[1] == 'ice':
         for stop in data_trip['trip']['stops']:
             if stop['station']['evaNr'] == stop_eva:
                 scheduledArrival = int(stop['timetable']['scheduledArrivalTime']) / 1000
+
+                time_to_arrival = (scheduledArrival + 60 * float(stop['timetable']['arrivalDelay'])) - time.time()
+                minutes_to_arrival = time_to_arrival / 60
+
+                if stop['station']['name'] in notification_stops and minutes_to_arrival < notification_minutes:
+                    if subprocess.run(["dunstctl", "is-paused"], capture_output=True).stdout == b'true\n':
+                        print("resuming dunstify")
+                        subprocess.run(['/home/julgoe/myConfigFiles/i3scripts/dunstToggle.sh', 'resume'])
+                    else:
+                        print("dunstify already running")
+
+                    subprocess.run(["dunstify",
+                                    "-r", "7777",  # to replace old ones
+                                    "Stop in {} in {:.0f} minutes".format(
+                                        stop['station']['name'],
+                                        minutes_to_arrival,
+                                    ),
+                                    ])
+
                 scheduledArrival_date = datetime.datetime.fromtimestamp(scheduledArrival)
                 return ", next: {} at {}{} on track {}".format(
                     stop['station']['name'],
