d3752d622e183d4da7439c57a7298bc96a560459
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2021-12-16 20:16:43 +0100

    more verbose db_wifi

--- i3scripts/db_wifi.py
+++ i3scripts/db_wifi.py
@@ -11,35 +11,58 @@ import time
 notification_minutes = 5
 notification_stops = [
     "Mannheim Hbf",
+    "Stuttgart Hbf",
+    "Crailsheim",
 ]
 
 if len(sys.argv) == 1 or sys.argv[1] == 'ice':
     base = 'https://portal.imice.de/api1/rs/'
 
     try:
-        data_status = requests.get(base + 'status').json()
-        data_trip = requests.get(base + 'tripInfo/trip').json()
-    except json.decoder.JSONDecodeError:
+        data_status = requests.get(
+            base + 'status',
+        ).json()
+        data_trip = requests.get(
+            base + 'tripInfo/trip',
+        ).json()
+    except json.decoder.JSONDecodeError as e:
         print("[connected to ICE: error with json]")
-        sys.exit()
-    except requests.exceptions.ConnectionError:
-        print("[connected to ICE: error connecting to portal]")
-        sys.exit()
+        raise e
+    except requests.exceptions.ConnectionError as e:
+        # import urllib3
+        # urllib3.disable_warnings()
+        print("found the following exception", file=sys.stderr)
+        print(e, file=sys.stderr)
+        print("retrying without verification of ssl certificate", file=sys.stderr)
+        try:
+            data_status = requests.get(
+                base + 'status',
+                verify=False,
+            ).json()
+            data_trip = requests.get(
+                base + 'tripInfo/trip',
+                verify=False
+            ).json()
+        except requests.exceptions.ConnectionError as e:
+            print("[connected to ICE: error connecting to portal]")
+            raise e
 
     def nextstopinfo(stop_eva):
         for stop in data_trip['trip']['stops']:
             if stop['station']['evaNr'] == stop_eva:
                 scheduledArrival = int(stop['timetable']['scheduledArrivalTime']) / 1000
 
-                time_to_arrival = (scheduledArrival + 60 * float(stop['timetable']['arrivalDelay'])) - time.time()
+                arrivalDelay = stop['timetable']['arrivalDelay']
+                arrivalDelay = 0 if arrivalDelay == '' else float(arrivalDelay)
+                time_to_arrival = (scheduledArrival + 60 * arrivalDelay) - time.time()
                 minutes_to_arrival = time_to_arrival / 60
 
                 if stop['station']['name'] in notification_stops and minutes_to_arrival < notification_minutes:
                     if subprocess.run(["dunstctl", "is-paused"], capture_output=True).stdout == b'true\n':
-                        print("resuming dunstify")
-                        subprocess.run(['/home/julgoe/myConfigFiles/i3scripts/dunstToggle.sh', 'resume'])
+                        print("resuming dunstify", file=sys.stderr)
+                        subprocess.run(['/home/julgoe/myConfigFiles/i3scripts/dunstToggle.sh', 'resume'],)
                     else:
-                        print("dunstify already running")
+                        print("dunstify already running", file=sys.stderr)
 
                     subprocess.run(["dunstify",
                                     "-r", "7777",  # to replace old ones
