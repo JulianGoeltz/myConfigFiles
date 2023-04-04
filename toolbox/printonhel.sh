b5d600f8488a191756b6d9785ebaf153984d4759
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2023-04-04 14:31:00 +0200

    for printonhel use trimmed filenames

--- toolbox/printonhel.sh
+++ toolbox/printonhel.sh
@@ -7,10 +7,12 @@ if [ $# -eq 0 ]; then
 	echo "there should be an argument"
 	exit 1
 fi
-# copy all files to tmp folder on wang
-scp "$@" hel:printing/tmp/
 # for each file given as argument
 for file in "$@"; do
+	# cleaned filename
+	fileTrimmed="$(echo "$file" | sed 's/[^0-9A-Za-z.-]//g')"
+	# copy file to tmp folder on wang
+	scp "$file" hel:printing/tmp/"$fileTrimmed"
 	# print and delete file
-	ssh hel 'lp -U jgoeltz -o fit-to-page -o sides=two-sided-long-edge -o media=A4 -h printer.kip.uni-heidelberg.de -d r1 "printing/tmp/'$file'"; rm "printing/tmp/'$file'";';
+	ssh hel 'lp -U jgoeltz -o fit-to-page -o sides=two-sided-long-edge -o media=A4 -h printer.kip.uni-heidelberg.de -d r1 "printing/tmp/'"$fileTrimmed"'"; rm "printing/tmp/'"$fileTrimmed"'";';
 done
