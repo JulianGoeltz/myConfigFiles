88b66530f433baede29caba15fabe32cbafd6470
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2024-01-02 18:45:55 +0100

    other scanartifact parameters

--- toolbox/scanArtifacts.sh
+++ toolbox/scanArtifacts.sh
@@ -3,7 +3,8 @@
 # create artifacts that look like a scan
 
 
-# very bad scan:
-# convert -density 150 $1 -colorspace gray \( +clone -blur 0x1 \) +swap -compose divide -composite -linear-stretch 5%x0% -rotate 1.5 scan.pdf
+# quality of the two calls is differen
 
-convert -density 150 $1 -colorspace gray  -blur 0x0.8 -attenuate 0.25 -linear-stretch 3.5%x10% -rotate 0.5 scan.pdf
+convert -density 150 "$1" -colorspace gray \( +clone -blur 0x1 \) +swap -compose divide -composite -linear-stretch 5%x0% -rotate 1.5 scan.pdf
+
+# convert -density 150 "$1" -colorspace gray  -blur 0x0.8 -attenuate 0.25 -linear-stretch 3.5%x10% -rotate 0.5 scan.pdf
